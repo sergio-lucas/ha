c6c9ae2e73f49552a8fda7a9a0fafcfc1d573e9d
sergey.lukjanchuk <sergey.lukjanchuk@devellar.com>
2015-11-09 18:20:13 +0200

    fix responsive view

--- sedona/dist/js/main.js
+++ sedona/dist/js/main.js
@@ -2,8 +2,7 @@
   var toggleShow = document.querySelector('.js-toggleShow'),
       startBtnCalc = document.getElementById('start-value'),
       startBtnCalc = document.getElementById('end-value'),
-      toogleForm = document.querySelector('.js-show'),
-      toogleForm = document.querySelector('.price-night__toggle');
+      toogleForm = document.querySelector('.js-show');
 
   toggleShow.addEventListener('click', function (e) {
     e.preventDefault();
